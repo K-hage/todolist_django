f98a60eba81db1e5b20cdf88cd0a7611120f927a
KHAGE <khazhanyants@mail.com>
2022-12-29 02:13:10 +0300

    create urls, update ignorelists

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from core.views import (
+    LoginView,
+    ProfileView,
+    RegistrationView,
+    UpdatePasswordView
+)
+
+
+urlpatterns = [
+    path('signup', RegistrationView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('update_password', UpdatePasswordView.as_view(), name='update-password'),
+]
