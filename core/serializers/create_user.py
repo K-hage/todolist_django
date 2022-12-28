c4d90c2ce728306da2494080c8638dd967609400
KHAGE <khazhanyants@mail.com>
2022-12-29 02:07:01 +0300

    create serializers

---
+++ core/serializers/create_user.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import (
+    exceptions,
+    serializers
+)
+
+from core.models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_repeat = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'password_repeat',
+        )
+
+    def create(self, validated_data):
+        password = validated_data.get('password')
+        password_repeat = validated_data.pop('password_repeat', None)
+
+        if password != password_repeat:
+            raise exceptions.ValidationError('Пароли не совпадают')
+
+        validated_data['password'] = make_password(password)
+
+        user = super().create(validated_data)
+        return user
