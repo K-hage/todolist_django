b2c710f631dee073285cbb6b22158bb179becf2c
KHAGE <khazhanyants@mail.com>
2023-01-01 23:44:37 +0300

    create models for goals

--- goals/models.py
+++ goals/models.py
@@ -1,2 +1,21 @@
+from django.db import models
 
-# Create your models here.
+from core.models import User
+
+
+class DatesModelMixin(models.Model):
+    class Meta:
+        abstract = True  # Помечаем класс как абстрактный – для него не будет таблички в БД
+
+    created = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Дата последнего обновления', auto_now=True)
+
+
+class GoalCategory(DatesModelMixin):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    title = models.CharField(verbose_name='Название', max_length=255)
+    user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name='Удалена', default=False)
