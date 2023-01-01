273d1236d2ef7693ab539c117410b8815a6f9804
KHAGE <khazhanyants@mail.com>
2023-01-02 00:02:59 +0300

    create all for list goals

--- goals/views.py
+++ goals/views.py
@@ -1,11 +1,28 @@
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView
+)
 
 from goals.models import GoalCategory
-from goals.serializers import GoalCreateSerializer
+from goals.serializers import (
+    GoalCategorySerializer,
+    GoalCreateSerializer
+)
 
 
 class GoalCategoryCreateView(CreateAPIView):
     model = GoalCategory
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = GoalCreateSerializer
+
+
+class GoalCategoryListView(ListAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCategorySerializer
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(
+            user=self.request.user, is_deleted=False
+        )
