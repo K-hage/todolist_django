b2bc4d00637eaacb904e8de18a524b76acfba0e0
KHAGE <khazhanyants@mail.com>
2023-01-01 23:50:49 +0300

    create view and urls for create goals

--- goals/views.py
+++ goals/views.py
@@ -1,2 +1,11 @@
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from goals.models import GoalCategory
+from goals.serializers import GoalCreateSerializer
+
+
+class GoalCategoryCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCreateSerializer
