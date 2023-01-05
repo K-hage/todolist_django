1e1b227b10a56b075963b9e1933b3dbefc2019e1
KHAGE <khazhanyants@mail.com>
2023-01-06 01:46:12 +0300

    create admin for goal and comment for goal

--- goals/admin.py
+++ goals/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory
+from goals.models import (
+    Goal,
+    GoalCategory,
+    GoalComment
+)
 
 
 @admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'user')
+
+
+admin.site.register(Goal)
+admin.site.register(GoalComment)
