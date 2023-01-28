dbd2b535d13359e279ab9012653bc4fa86174d3e
KHAGE <khazhanyants@mail.com>
2023-01-29 01:05:55 +0300

    add drf-spectacular

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,6 +3,12 @@ from django.urls import (
     include,
     path
 )
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
+
+from todolist.settings import DOMAIN_SITE
 
 
 urlpatterns = [
@@ -11,4 +17,6 @@ urlpatterns = [
     path('oauth/', include('social_django.urls', namespace='social')),
     path('goals/', include('goals.urls')),
     path('bot/', include('bot.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url=f'http://{DOMAIN_SITE}/api/schema/'), name='swagger-ui'),
 ]
